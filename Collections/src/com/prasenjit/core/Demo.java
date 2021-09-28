d04efeb6ff1fab8a728f9564c03fc9e715af1d2a
pmalakar09 <pmalakar09@gmail.com>
2021-09-28 17:15:07 +0530

    Done

---
+++ Collections/src/com/prasenjit/core/Demo.java
@@ -0,0 +1,50 @@
+package com.prasenjit.core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> objAL = new ArrayList<Integer>();
+		objAL.add(10);
+		objAL.add(11);
+		objAL.add(12);
+		objAL.add(15);
+		objAL.add(2);
+		System.out.println(objAL);
+		System.out.println(objAL.get(2));
+		Collections.sort(objAL);
+		System.out.println(objAL);
+		Collections.sort(objAL, Collections.reverseOrder());
+		System.out.println(objAL);
+		
+		HashSet<Integer> objHS= new HashSet<Integer>();
+		objHS.add(5);
+		objHS.add(6);
+		objHS.add(7);
+		objHS.add(8);
+		objHS.add(100);
+		objHS.add(5);
+		System.out.println(objHS);
+		
+		HashMap<Integer, String> objHM= new HashMap<Integer, String>();
+		objHM.put(1, "Prasenjit");
+		objHM.put(2, "ABC");
+		objHM.put(3, "XYX");
+		objHM.put(null, "LP");
+		System.out.println(objHM);
+		
+		TreeMap<Integer, String> objTM=new TreeMap<Integer, String>();
+		objTM.put(1, "A");
+		objTM.put(2, "B");
+		objTM.put(3, "C");
+		objTM.put(4, "D");
+		System.out.println(objTM);
+
+	}
+
+}
--- Test/src/Demo/Demo.java
+++ Test/src/Demo/Demo.java
@@ -1,19 +1,22 @@
 package Demo;
 
-class Demo1
-{
-int a =30;	
-void disp()
-{
-	System.out.println(a);
-}
-}
-class Demo extends Demo1
-{
- public static void main(String []args)
- {
-	 int a=300;
-	 Demo1 d=new Demo1();
-	 d.disp();
- }
+import java.util.ArrayList;
+
+class Demo {
+
+	public static void main(String[] args) {
+		int i[] = { 1, 2, 3, 4, 5, 6, 8, 7, 4, 3, 1, 2 };
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		for (int j = 0; j < i.length; j++) {
+			for (int k = j + 1; k < i.length; k++) {
+				if (i[j] == i[k]) {
+					// System.out.println(i[j]);
+					res.add(i[j]);
+				}
+
+			}
+
+		}
+		System.out.println(res);
+	}
 }
